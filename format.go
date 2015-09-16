bc8e515b79b3efcf1caeeca2fddd1b79d0ad414f
aholic <ruochen.xu@gmail.com>
2015-09-16 15:41:34 +0800

    add "

--- format.go
+++ format.go
@@ -11,19 +11,19 @@ func JsonFmtHandleFunc(w http.ResponseWriter, req *http.Request) {
 	defer req.Body.Close()
 	body, err := ioutil.ReadAll(req.Body)
 	if err != nil {
-		w.Write([]byte(`{"ok": "no", "msg": ` + err.Error() + `}`))
+		w.Write([]byte(`{"ok": "no", "msg": "` + err.Error() + `"}`))
 		return
 	}
 
 	formatted, err := Format(body)
 	if err != nil {
-		w.Write([]byte(`{"ok": "no", "msg": ` + err.Error() + `}`))
+		w.Write([]byte(`{"ok": "no", "msg": "` + err.Error() + `"}`))
 		return
 	}
 
-	w.Write([]byte(`{"ok": "yes", "msg": `))
+	w.Write([]byte(`{"ok": "yes", "msg": "`))
 	w.Write(formatted)
-	w.Write([]byte(`}`))
+	w.Write([]byte(`"}`))
 }
 
 func Format(content []byte) ([]byte, error) {
