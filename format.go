cc70b60e8313b50d4536e30a0b8ffaeef9d94bc0
aholic <ruochen.xu@gmail.com>
2015-09-16 14:22:18 +0800

    add http handler for Format

--- format.go
+++ format.go
@@ -3,7 +3,28 @@ package gojsonfmt
 import (
 	"bytes"
 	"encoding/json"
+	"io/ioutil"
+	"net/http"
 )
+
+func JsonFmtHandleFunc(w http.ResponseWriter, req *http.Request) {
+	defer req.Body.Close()
+	body, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		w.Write([]byte(`{"ok": "no", "msg": ` + err.Error() + `}`))
+		return
+	}
+
+	formatted, err := Format(body)
+	if err != nil {
+		w.Write([]byte(`{"ok": "no", "msg": ` + err.Error() + `}`))
+		return
+	}
+
+	w.Write([]byte(`{"ok": "yes", "msg": `))
+	w.Write(formatted)
+	w.Write([]byte(`}`))
+}
 
 func Format(content []byte) ([]byte, error) {
 	dst := new(bytes.Buffer)
